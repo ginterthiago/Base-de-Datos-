f727fc2e9f700a2bbd3206227e0284c2e12a3c11
ginterthiago <ginterthiago@gmail.com>
2026-05-11 19:51:51 -0300

    Create Base de Datos - primer año.session.sql

---
+++ Base de Datos - primer año.session.sql
@@ -0,0 +1,4 @@
+CREATE TABLE 
+insert into `Stock` (CANT, MARCA, PRECIO) VALUES (10, 'Samsung', 99.99); 
+insert into `Stock` (CANT, MARCA, PRECIO) VALUES (5, 'Apple', 199.99); 
+insert into `Stock` (CANT, MARCA, PRECIO) VALUES (20, 'Xiaomi', 49.99); 
